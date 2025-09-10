54549ff171aabd65041592b9b954e5177ab949e3
MCborgurdud <mylesmmadden@gmail.com>
2025-09-09 20:16:09 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,1 +1,19 @@
 
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val textView = TextView(this).apply {
+            text = "Hello"
+            textSize = 24f
+        }
+
+        setContentView(textView)
+    }
+}
