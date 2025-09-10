5ae5b34ba4b17bb4c28ea29bf52bde81ced8b576
MCborgurdud <mylesmmadden@gmail.com>
2025-09-10 17:35:06 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,40 +1,35 @@
 package com.example.notes
 
-import com.example.notes.NoteAdapter
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var noteAdapter: NoteAdapter
     private val notes = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val inputNote = findViewById<EditText>(R.id.inputNote)
-        val addButton = findViewById<Button>(R.id.addButton)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         noteAdapter = NoteAdapter(notes) { note ->
             notes.remove(note)
             noteAdapter.notifyDataSetChanged()
         }
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = noteAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = noteAdapter
 
-        addButton.setOnClickListener {
-            val note = inputNote.text.toString()
+        binding.addButton.setOnClickListener {
+            val note = binding.inputNote.text.toString()
             if (note.isNotEmpty()) {
                 notes.add(note)
                 noteAdapter.notifyDataSetChanged()
-                inputNote.text.clear()
+                binding.inputNote.text.clear()
             }
         }
     }
